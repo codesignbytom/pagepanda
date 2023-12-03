d1f6d37ce11c5002204b666d5e65d103a955c0cf
Tom Graafmans <graafmans.tom@gmail.com>
2023-12-03 18:23:08 +0100

    update 1 file and create 1 file

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_URL: string = "https://pagepanda.netlify.app/";
+export const SITE_NAME: string = "PagePanda";
+export const SITE_DESCRIPTION: string =
+  "PagePanda is a website builder that allows you to build high converting landing pages in minutes";
