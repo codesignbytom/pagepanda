a82c6ff3c84bcc217d8122ec85cef35ce6265545
Tom Graafmans <graafmans.tom@gmail.com>
2023-12-07 11:30:46 +0100

    update RadialGradientCircle.jsx

--- src/components/atoms/RadialGradientCircle.jsx
+++ src/components/atoms/RadialGradientCircle.jsx
@@ -5,7 +5,7 @@ const RadialGradientCircle = ({
 }) => {
   return (
     <div
-      className={`${classes} absolute w-[32rem] sm:w-[36rem] md:w-[40rem] lg:w-[48rem] xl:w-[56rem] h-auto aspect-square -z-20 bg-gradient-radial from-${fromColor} rounded-full blur-[6rem]`}
+      className={`${classes} absolute w-[32rem] sm:w-[36rem] md:w-[40rem] lg:w-[48rem] xl:w-[56rem] 2xl:w-[64rem] h-auto aspect-square -z-20 bg-gradient-radial from-${fromColor} rounded-full blur-[6rem]`}
       style={{
         opacity: opacity / 100,
       }}
