b44922ddc37719b3dda4b24e8f39e2b62bafeffa
Tom Graafmans <graafmans.tom@gmail.com>
2023-12-07 10:26:41 +0100

    update Button.jsx

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,6 +1,7 @@
 const Button = ({ text = "", link, classes, icon = "", iconClasses = "" }) => {
+  const buttonClass = icon ? `${classes} button !py-3.5` : `${classes} button`;
   return (
-    <a className={`${classes} button`} href={link || "#"}>
+    <a className={buttonClass} href={link || "#"}>
       {icon && <i className={`ph-light ph-${icon} ${iconClasses}`}></i>}
       {text && <span>{text}</span>}
     </a>
